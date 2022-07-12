e0703c211b8dc6d2bb557d6bf6ca2af6d779d9eb
Eduardo Franz <eduardo.franz77@gmail.com>
2022-07-12 00:36:23 -0300

    -add camadas security e service

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,95 @@
+package com.generation.blogpessoal.model;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/* para essa classe ser interpretada pelo banco de dados,para o jpa/hibernate consiga entender como uma entidade
+precisamos inserir algumas anotações,
+anotações são parametros que colocamos em cima das classes e proproeidades que definem uma certa tipo de comportamento*/
+
+
+@Entity //ele indica essa classe como entidade do jpa/hibernate
+@Table(name = "tb_postagem")  //que essa entidade dentro do banco de dados vai virar uma tabela
+public class Postagem {
+
+	@Id //definir o id da tabela
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // esse atributo vai ser tornar primary key
+	private long id;
+	
+	@NotNull//o campo titulo não pode ser nulo
+	@Size(min = 5, max = 100) //determinara quantidade de caracteres que o cliente consegue enviar como titulo
+	private String titulo;
+	
+	@NotNull//o campo texto não pode ser nulo
+	@Size(min = 5, max = 500) //determinara quantidade de caracteres que o cliente consegue enviar como texto
+	private String texto;
+	
+	
+	//@UpdateTimestamp -> Anotação que pega automaticamente hora e data do seu computador
+	@Temporal(TemporalType.TIMESTAMP)  //indicar para o jpa/hibernate que estamos trabalhando com tempo
+	private Date data = new java.sql.Date(System.currentTimeMillis()); //para capturar exatamente a data, quando um dado passar por essa clase
+	//trocar Date por LocalDateTime data,maneira diferente de testar data
+	
+	
+	//Relacionamento entre as tabelas, fazer com que tema e postagem crie relacionamento
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	
+	
+	//getters e setters da aplicação
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	
+	
+	
+	
+}
---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.generation.blogpessoal.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//para não dar warning
+//@SuppressWarnings("deprecation")
+//@EnableWebSecurity
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+ 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+     }
+	
+	/*
+	 * criando o usuario em memoria, ter acesso aos endpoints e modificalos,
+	 * apenas para testes, quando terminar a aplicação tira
+	 *
+	 * auth.inMemoryAuthentication()
+	 * .withUser("root")
+	 * .password(passwordEncoder().encode ("root"))
+	 * .authorities("ROLE_USER");
+	 */
+	
+	
+	  @Bean
+	  public PasswordEncoder passwordEncoder(){
+	  return new BCryptPasswordEncoder();
+	  }
+	 
+	
+	   
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception{
+	          http.authorizeRequests()
+	         .antMatchers("/usuario/logar").permitAll()
+	     	 .antMatchers("/usuario/cadastrar").permitAll()
+	         .antMatchers(HttpMethod.OPTIONS).permitAll()
+	         .anyRequest().authenticated()
+	         .and().httpBasic()
+	         .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //stateless não vai guardar sessão
+	         .and().cors()
+	         .and().csrf().disable(); 
+	  }
+}
